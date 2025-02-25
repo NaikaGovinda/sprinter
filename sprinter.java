80e9685d7e812013840f52699045c21326f09667
Naika <naikagovinda@gmail.com>
2025-02-25 12:24:12 +0700

    Добавил

---
+++ sprinter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class sprinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of your student");
+
+    }
+}
